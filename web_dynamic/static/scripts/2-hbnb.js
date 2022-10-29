b52df46d88825272484c4e653b82bc0d9e1c65ca
thala254 <thalamine@gmail.com>
2022-10-29 16:20:03 +0300

    added compulsory tasks

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,5 @@
 $(() => {
-    $.get('http://127.0.0.1:5001/api/v1/status/', data => {
+    $.get('http://0.0.0.0:5001/api/v1/status/', data => {
       if (data.status === 'OK') {
         $('#api_status').addClass('available');
       } else {
@@ -15,7 +15,7 @@ $(() => {
       }
       const values = Object.values(listCheck);
       const list = values.join(', ');
-      const short = list.slice(0, 34);
+      const short = list.slice(0, 30);
       $('.amenities h4').html(short.length > 0 ? `<em>${short}</em>` : '&nbsp;');
     });
   });
